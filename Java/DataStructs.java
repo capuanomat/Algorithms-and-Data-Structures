d44f405cae5c79a05385bf1bdef3f5d512f3fcf9
Matthieu J. Capuano <capuanomat@gmail.com>
2018-10-07 22:55:54 -0400

    Added Java notes from Language References

---
+++ Java/DataStructs.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Collections;
+import java.util.HashMap;
+/**
+ * Created by Matthieu J.B Capuano on 10/25/2017.
+ */
+public class DataStructs {
+
+
+}
